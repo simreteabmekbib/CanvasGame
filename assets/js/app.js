3e3f93a06bcbcc48b5b937aae81fa56c5a75d5a9
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-02-20 08:16:27 -0800

    replaced with ES6 class

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,8 @@ start.addEventListener('click', function(){
     localStorage.setItem('computer score', com.score);
 
     game();
-    let loop = setInterval(game,1000/framePerSecond);
+    let loop = setInterval(game,1000/framePerSecond);//FPS is used to measure frame rate – the number of consecutive full-screen images that are displayed each second.
+     // 50 times every one second
 });
 startPre.addEventListener('click',function(){
     comp_score = JSON.parse(localStorage.getItem('computer score'));
@@ -29,10 +30,10 @@ startPre.addEventListener('click',function(){
 const ctx = canvas.getContext('2d');
 
 // load sounds
-let hit = new Audio();
-let wall = new Audio();
-let userScore = new Audio();
-let comScore = new Audio();
+let hit = new Audio(); //paddel
+let wall = new Audio();  // top and bottom
+let userScore = new Audio(); // for comp side score
+let comScore = new Audio();  // for user side score
 
 hit.src = "sounds/hit.mp3";
 wall.src = "sounds/wall.mp3";
@@ -51,33 +52,23 @@ const ball = {
 }
 
 // User Paddle
-const user = {
-    x : 0, // left side of canvas
-    y : (canvas.height - 100)/2, // -100 the height of paddle
-    width : 10,
-    height : 100,
-    score : 0,
-    color : "WHITE"
+class Layout{
+    constructor(x,y,width,height,score,color){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.score= score;
+        this.color=color;
+    }
 }
+const user = new Layout(0,(canvas.height - 100)/2,10,100,0,"WHITE");
 
 // COM Paddle
-const com = {
-    x : canvas.width - 10, // - width of paddle
-    y : (canvas.height - 100)/2, // -100 the height of paddle
-    width : 10,
-    height : 100,
-    score : 0,
-    color : "WHITE"
-}
+const com = new Layout(canvas.width - 10,(canvas.height - 100)/2,10,100,0,"WHITE");
 
 // NET
-const net = {
-    x : (canvas.width - 2)/2,
-    y : 0,
-    height : 10,
-    width : 2,
-    color : "WHITE"
-}
+const net = new Layout((canvas.width - 2)/2,0,2,10,null,"WHITE");
 
 // draw a rectangle, will be used to draw paddles
 function drawRect(x, y, w, h, color){
@@ -89,7 +80,7 @@ function drawRect(x, y, w, h, color){
 function drawArc(x, y, r, color){
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI*2,true);
+    ctx.arc(x,y,r,0,Math.PI*2,true);// boolean if for the direction of drawing if true counter clockwise //oposite of clock direction
     ctx.closePath();
     ctx.fill();
 }
@@ -100,7 +91,7 @@ canvas.addEventListener("mousemove", getMousePos);
 function getMousePos(evt){
     let rect = canvas.getBoundingClientRect();
     
-    user.y = evt.clientY - rect.top - user.height/2;
+    user.y = evt.clientY - rect.top - user.height/2; // clientY tells the vertical position of the mouse
 }
 
 // when COM or USER scores, we reset the ball
@@ -141,7 +132,7 @@ function collision(b,p){
 }
 
 // update function, the function that does all calculations
-function update(){
+function update(){//the game start its direction will be in the positive direction
      
     // change the score of players, if the ball goes to the left "ball.x<0" computer win, else if "ball.x > canvas.width" the user win
     if( ball.x - ball.radius < 0 ){
@@ -172,12 +163,12 @@ function update(){
     }
     
     // we check if the paddle hit the user or the com paddle
-    let player = (ball.x + ball.radius < canvas.width/2) ? user : com;
+    let player = (ball.x + ball.radius < canvas.width/2) ? user : com; // if else
     
     // if the ball hits a paddle
-    if(collision(ball,player)){
+    if(collision(ball,player)){ // player is paddel
         // play sound
-        hit.play();
+        hit.play();// with paddel
         // we check where the ball hits the paddle
         let collidePoint = (ball.y - (player.y + player.height/2));
         // normalize the value of collidePoint, we need to get numbers between -1 and 1.
@@ -188,11 +179,11 @@ function update(){
         // when the ball hits the center of the paddle we want the ball to take a 0degrees angle
         // when the ball hits the bottom of the paddle we want the ball to take a 45degrees
         // Math.PI/4 = 45degrees
-        let angleRad = (Math.PI/4) * collidePoint;
+        let angleRad = (Math.PI/4) * collidePoint; //range -1 -1 times 45deg
         
         // change the X and Y velocity direction
         let direction = (ball.x + ball.radius < canvas.width/2) ? 1 : -1;
-        ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+        ball.velocityX = direction * ball.speed * Math.cos(angleRad); //if ball hit the user paddel dirextion +ve else -ve
         ball.velocityY = ball.speed * Math.sin(angleRad);
         
         // speed up the ball everytime a paddle hits it.
@@ -202,7 +193,7 @@ function update(){
 
 // render function, the function that does al the drawing
 function render(){
-    
+     //when we call render screen clear and start from the updated position
     // clear the canvas
     drawRect(0, 0, canvas.width, canvas.height, "#000");
     
